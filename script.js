18985c8687c6bee87ad61bf8b52280368f7293b3
David Smoot <dsmoot@wlapcs.org>
2017-04-11 16:34:48 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<p>Aye homie look at my code i don't feel like doing this to be honest.</p>
+
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+   background(255, 255, 0);
+};
---
+++ style.css
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(600,400);
+    david = new Pacman(300, 200);
+}
+
+function draw(){
+   background(0);
+}
